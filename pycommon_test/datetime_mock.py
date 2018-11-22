86a1d7a68103319be716c9ab9eed5489c1bb9ba4
Colin Bounouar <colin.bounouar@external.engie.com>
2018-11-22 19:01:09 +0100

    Remove add and replace methods by a single one: set.

--- pycommon_test/datetime_mock.py
+++ pycommon_test/datetime_mock.py
@@ -9,6 +9,11 @@ class NowMock(datetime.datetime):
         return cls.date_time
 
 
-def mock_now(date_time: str='2018-10-11T15:05:05'):
+def mock_now(date_time: str='2018-10-11T15:05:05') -> None:
+    """
+    Mock datetime.now() function.
+
+    :param date_time: The datetime returned by datetime.now()
+    """
     NowMock.date_time = datetime.datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
     datetime.datetime = NowMock
